ab3ad0f4b4688a23e099453c97a4becc7fe605d6
Chang <cnxiong03@gmail.com>
2020-04-05 16:45:23 -0400

    looping through and outputting data

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'J.Cole', age: 35, belt: 'black', id: 1 },
+      { name: 'Kendrick', age: 34, belt: 'red', id: 2 },
+      { name: 'Abel', age: 33, belt: 'white', id: 3 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Chang's first React app!</h1>
         <p>Welcome User</p>
-        <Ninjas name='Chang' age='28' belt='Black'/>
-        <Ninjas name='Yoshi' age='33' belt='Blue'/>
+        <Ninjas ninjas= { this.state.ninjas }/>
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -11,12 +11,19 @@ class Ninjas extends Component {
         //     </div>
 
         //destructing is another way to store property
-        const { name, age, belt } = this.props;
+        const { ninjas } = this.props;
+        const ninjaList = ninjas.map(ninja => {
             return (
-                <div className='ninja'>
-                    <div>Name: { name }</div>
-                    <div>Age: { age }</div>
-                    <div>Belt: { belt }</div>
+            <div className='ninja' key={ ninja.id }>
+                <div>Name: { ninja.name }</div>
+                <div>Age: { ninja.age }</div>
+                <div>Belt: { ninja.belt }</div>
+            </div>
+            )
+        })
+            return (
+                <div className='ninja-list'>
+                    { ninjaList }
                 </div>
         )
     }
