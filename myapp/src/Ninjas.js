e0c78ce72798eea14e35c3aa4885342ee237f433
Chang <cnxiong03@gmail.com>
2020-04-05 16:58:58 -0400

    container component with state made by classes vs ui component without state made with functions

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,32 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    render () {
-        // console.log(this.props)
-        // return (
-        //     <div className='ninja'>
-        //         <div>Name: { this.props.name }</div>
-        //         <div>Age: { this.props.age }</div>
-        //         <div>Belt: { this.props.belt }</div>
-        //     </div>
-
-        //destructing is another way to store property
-        const { ninjas } = this.props;
-        const ninjaList = ninjas.map(ninja => {
-            return (
-            <div className='ninja' key={ ninja.id }>
-                <div>Name: { ninja.name }</div>
-                <div>Age: { ninja.age }</div>
-                <div>Belt: { ninja.belt }</div>
-            </div>
-            )
-        })
-            return (
-                <div className='ninja-list'>
-                    { ninjaList }
-                </div>
+const Ninjas = ({ ninjas }) => {
+    const ninjaList = ninjas.map(ninja => {
+        return (
+        <div className='ninja' key={ ninja.id }>
+            <div>Name: { ninja.name }</div>
+            <div>Age: { ninja.age }</div>
+            <div>Belt: { ninja.belt }</div>
+        </div>
         )
-    }
+    })
+        return (
+            <div className='ninja-list'>
+                { ninjaList }
+            </div>
+    )
 }
 
 export default Ninjas
