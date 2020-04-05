699052ad3f8680d8b7b4b8a78664eb8e2b5a847d
Chang <cnxiong03@gmail.com>
2020-04-05 17:17:41 -0400

    three ways to output our conditons

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,18 +1,46 @@
 import React from 'react';
 
 const Ninjas = ({ ninjas }) => {
-    const ninjaList = ninjas.map(ninja => {
-        return (
-        <div className='ninja' key={ ninja.id }>
-            <div>Name: { ninja.name }</div>
-            <div>Age: { ninja.age }</div>
-            <div>Belt: { ninja.belt }</div>
-        </div>
-        )
-    })
-        return (
-            <div className='ninja-list'>
-                { ninjaList }
+    // const ninjaList = ninjas.map(ninja => {
+    //     if ( ninja.age > 33){
+    //     return (
+    //     <div className='ninja' key={ ninja.id }>
+    //         <div>Name: { ninja.name }</div>
+    //         <div>Age: { ninja.age }</div>
+    //         <div>Belt: { ninja.belt }</div>
+    //     </div>
+    //     )
+    //   } else {
+    //       return null
+    //     }
+    // })
+
+// condition output without the if statement
+
+    // const ninjaList = ninjas.map(ninja => {
+    //     return ninja.age > 33 ? (
+    //     <div className='ninja' key={ ninja.id }>
+    //         <div>Name: { ninja.name }</div>
+    //         <div>Age: { ninja.age }</div>
+    //         <div>Belt: { ninja.belt }</div>
+    //     </div>
+    //     ) : null
+    // })
+   
+// embedding the condition in the return
+    return (
+        <div className='ninja-list'>
+            { 
+                ninjas.map(ninja => {
+                    return ninja.age > 33 ? (
+                        <div className='ninja' key={ ninja.id }>
+                            <div>Name: { ninja.name }</div>
+                            <div>Age: { ninja.age }</div>
+                            <div>Belt: { ninja.belt }</div>
+                        </div>
+                    ) : null;
+                })
+            }
             </div>
     )
 }
